b814c84bd5895c0ad80439a71afce1d728e45fe0
zhengqiangtan <844870676@qq.com>
2016-12-07 18:49:21 +0800

    first case about wordcount

--- src/main/com/spark/core/java/WordCountJava.java
+++ src/main/com/spark/core/java/WordCountJava.java
@@ -18,7 +18,7 @@ import java.util.Arrays;
  */
 public class WordCountJava {
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("").setMaster("local");
+        SparkConf conf = new SparkConf().setAppName("WordCountJava").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> lines = sc.textFile("c:\\spark.txt", 1);
