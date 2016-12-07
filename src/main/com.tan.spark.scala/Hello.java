af3130749245ab63aa49b2b7c7bd3b6f3c323315
zhengqiangtan <844870676@qq.com>
2016-12-07 16:37:07 +0800

    Initial commit

---
+++ src/main/com.tan.spark.scala/Hello.java
@@ -0,0 +1,11 @@
+package com.tan.spark.scala;
+
+/**
+ * 永远的HELLO WORLD (JAVA)
+ * Created by tzq on 2016/12/7.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Spark");
+    }
+}
---
+++ src/main/com.tan.spark.scala/HelloScala.scala
@@ -0,0 +1,10 @@
+package com.tan.spark.scala
+
+/**
+  * Created by tzq on 2016/12/7.
+  */
+object HelloScala {
+  def main(args: Array[String]): Unit = {
+    println("永远的Hello world")
+  }
+}
